fd6dc57b902430ed34ec195b83614d4cf19a559e
Renny Su <rennie.su@gmail.com>
2026-05-03 18:06:17 +0800

    Completed Chapter 11

--- 11_include/footer.php
+++ 11_include/footer.php
@@ -1,3 +1,8 @@
 <footer>
-    Contact Information
+    Footer - Contact Information
 </footer>
+
+</body>
+</html>
+
+<!-- browsers are forgiving - you can leave out the closing body and html tags -->
--- 11_include/index.php
+++ 11_include/index.php
@@ -5,3 +5,27 @@
 </main>
 
 <?php include 'footer.php' ?>
+
+<!-- can use
+
+require 'header.php';
+
+or
+
+require_once __DIR__ . '/header.php';
+
+as well... page will crash if not found
+
+This ensures:
+
+absolute-safe path resolution
+prevents double inclusion
+clearer error behavior
+
+require missing file → page crashes (fatal error)
+
+include missing file → page continues (warning only)
+
+And for critical layout files like headers/footers, require is often the more correct choice.
+
+-->
